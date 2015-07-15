5ccca065933c955a833c3a18212b670129439517
Yan Couto <yancouto@gmail.com>
2015-07-15 20:11:09 -0300

    Dia8-CEC

--- Yan/icpc/6284.cpp
+++ Yan/icpc/6284.cpp
@@ -21,16 +21,40 @@ inline ull mod(ull x) { return x % modn; }
 const int MAX = 300009;
 int n;
 ull xo[MAX];
+ull num[MAX];
 char str[MAX];
-int val(char c) {
+inline int val(char c) {
 	if(c >= 'a' && c <= 'z') return c - 'a';
 	else return c - 'A' + ('z' - 'a' + 1);
 }
 
 int main() {
-	int i;
-	scanf("%d %s", &n);
-	for(i = 1; i <= n; i++)
-		xo[i] = xo[i - 1] ^ (1ull << val(str[i - 1]));
-	sort(xo + 1, xo + n + 1);
+	int i, j;
+	while(scanf("%d %s", &n, str) != EOF) {
+		for(i = 1; i <= n; i++) {
+			xo[i] = xo[i - 1] ^ (1ull << val(str[i - 1]));
+		}
+		sort(xo + 1, xo + n + 1);
+		j = 0;
+		for(i = 0; i <= n; i++) {
+			xo[j] = xo[i];
+			if(j == 0 || xo[j] != xo[j - 1])
+				num[j++] = 1;
+			else num[j - 1]++;
+		}
+		n = j;
+		ull tot = 0;
+		for(j = 0; j < n; j++) {
+			for(i = 0; i < 52; i++) {
+				ull x = 1ull << i;
+				ull *l = lower_bound(xo, xo + n, x ^ xo[j]);
+				if(l != xo + n && *l == (x ^ xo[j]))
+					tot += num[l - xo] * num[j];
+			}
+			tot += (num[j] *(num[j] - 1));
+		}
+		tot /= 2;
+		printf("%llu\n", tot);
+	}
+	return 0;
 }
