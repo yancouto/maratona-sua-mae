26fddc9a51ece00380b8f8f35275c16f93435356
Yan Couto <yancouto@gmail.com>
2015-08-11 20:30:20 -0300

    codechef, ..

--- Yan/codeforces/468C.cpp
+++ Yan/codeforces/468C.cpp
@@ -1,63 +1,53 @@
-// WROOOONG
 #include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
 ll a;
-ll mod(ll x) { return x % a; }
 char str[200];
-int rnum() {
-	int n = 10;
-	for(int i = 0; i < n; i++) {
-		int num = rand() % 1000;
-		sprintf(str + i * 3, "%03d", num);
-	}
-	return n * 3;
-}
-map<ll, string> mp;
 int n;
 ll memo[30][2][302];
 ll solve(int dig, bool pre, int sum) {
-	if(dig == n) return mod(sum);
+	if(dig == n) return sum;
 	ll &r = memo[dig][pre][sum];
 	if(r != -1) return r;
 	r = 0;
 	for(int i = 0; i <= 9; i++) {
 		if(pre && i > (str[dig] - '0')) break;
-		r = mod(r + solve(dig + 1, pre && (i == (str[dig] - '0')), sum + i));
+		r += solve(dig + 1, pre && (i == (str[dig] - '0')), sum + i);
 	}
 	return r;
 }
 
-char str2[200];
+int f(ll x) {
+	int s = 0;
+	while(x) {
+		s += x % 10;
+		x /= 10;
+	}
+	return s;
+}
 
 int main() {
-	srand(time(NULL)); rand(); rand();
-	scanf("%lld", &a);
-	//scanf("%s %s", str, str2);
-	//memset(memo, -1, sizeof memo); n = strlen(str);
-	//ll aa = solve(0, true, 0);
-	//strcpy(str, str2);
-	//memset(memo, -1, sizeof memo); n = strlen(str);
-	//ll bb = solve(0, true, 0);
-	//printf("%lld - %lld = %lld\n", bb, aa, bb - aa);
-	//return 0;
+	scanf("%I64d", &a);
 
-	while(true) {
-		n = rnum();
+	ll l = 1, r = 100000000000000000ll;
+	ll s;
+	while(l < r) {
+		ll m = (l + r) / 2;
+		sprintf(str, "%I64d", m); n = strlen(str);
 		memset(memo, -1, sizeof memo);
-		ll s = solve(0, true, 0);
-		if(mp.count(s)) {
-			strcpy(str2, mp[s].c_str());
-			char *s1 = str, *s2 = str2;
-			if(strcmp(s1, s2) > 0) swap(s1, s2);
-			int i = n - 1;
-			while(s1[i] == '9') s1[i--] = '0';
-			s1[i]++;
-			while(s1[0] == '0') s1++;
-			while(s2[0] == '0') s2++;
-			printf("%s %s\n", s1, s2);
-			return 0;
-		}
-		mp[s] = string(str);
+		s = solve(0, true, 0);
+		assert(s > 0);
+		if(s >= a) r = m;
+		else l = m + 1;
 	}
+	sprintf(str, "%I64d", l); n = strlen(str);
+	memset(memo, -1, sizeof memo);
+	s = solve(0, true, 0) % a;
+	l = 1;
+	while(s) {
+		if(s > 0) s -= f(l++);
+		else s += f(++r);
+	}
+	printf("%I64d %I64d\n", l, r);
+	return 0;
 }
--- Yan/codeforces/496E.cpp
+++ Yan/codeforces/496E.cpp
@@ -1,22 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
+#define fst first
+#define snd second
 const int MAX = 100009;
 int ps[MAX][2], as[MAX][3];
 map<int, int> compr;
 
-vector<int> padd[MAX], aadd[MAX];
-multimap<int, int> mp;
+vector<int> padd[MAX << 2], aadd[MAX << 2];
+multimap<int, pair<int, int> > mp;
+int resp[MAX];
 
 int main() {
-	int i;
+	int i, n, m;
 	scanf("%d", &n);
 	for(i = 0; i < n; i++) {
 		scanf("%d %d", &ps[i][0], &ps[i][1]);
 		compr[ps[i][0]] = 0; compr[ps[i][1]] = 0;
 	}
 	scanf("%d", &m);
-	for(i = 0; i < n; i++) {
-		scanf("%d", &as[i][0], &as[i][1], &as[i][2]);
+	for(i = 0; i < m; i++) {
+		scanf("%d %d %d", &as[i][0], &as[i][1], &as[i][2]);
 		compr[as[i][0]] = 0; compr[as[i][1]] = 0;
 	}
 	i = 0;
@@ -33,11 +36,22 @@ int main() {
 		aadd[as[i][0]].push_back(i);
 	}
 	for(i = 0; i < compr.size(); i++) {
-		while(!mp.empty() && mp.begin().fst < i) mp.erase(mp.begin());
-		for(int v : aadd) mp.insert(make_pair(as[v][1], as[v][2]));
-		for(int v : padd) {
+		while(!mp.empty() && mp.begin()->fst < i) mp.erase(mp.begin());
+		for(int v : aadd[i]) mp.insert(make_pair(as[v][1], make_pair(as[v][2], v)));
+		for(int v : padd[i]) {
 			auto it = mp.lower_bound(ps[v][1]);
-			/* terminar */
+			if(it == mp.end()) {
+				puts("NO");
+				return 0;
+			}
+			resp[v] = it->snd.snd;
+			it->snd.fst--;
+			if(it->snd.fst == 0) mp.erase(it);
 		}
 	}
+	puts("YES");
+	for(i = 0; i < n; i++)
+		printf("%d ", resp[i] + 1);
+	putchar('\n');
+	return 0;
 }
