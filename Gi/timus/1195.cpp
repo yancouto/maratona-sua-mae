64df2b3278bae10bdc4b83aa54a2231af9ac95e2
Giovana Gomes Delfino <gidelfino@hubble.eclipse.ime.usp.br>
2015-10-08 20:54:18 -0300

    e selvas

--- Gi/timus/1195.cpp
+++ Gi/timus/1195.cpp
@@ -11,72 +11,73 @@ template<typename T> inline T abs(T t) { return t < 0? -t : t; }
 const ll modn = 1000000007;
 inline ll mod(ll x) { return x % modn; }
 
-int l[3][2], c[3][2], d1[2], d2[2];
 char tab[3][3];
 
-bool checklin(int cc, char let) {
-	for(int i = 0; i < 3; i++)
-		if(l[i][cc] == 2 && !l[i][!cc]) { 
-			int j; for(j = 0; j < 3 && tab[i][j] != '#');
-			tab[i][j] = let;
-			return true;
+bool check(int p) {
+	int cr, ot;
+	int d1r, d1o, d2r, d2o;
+	d1r = d1o = d2r = d2o = 0;
+	for(int i = 0; i < 3; i++) {
+		cr = ot = 0;
+		for(int j = 0; j < 3; j++) {
+			cr += tab[i][j] == 'X';
+			ot += tab[i][j] == 'O';
+			if(i == j) { d1r += (tab[i][j] == 'X'); d1o += (tab[i][j] == 'O'); }
+			if(i+j == 2) { d2r += (tab[i][j] == 'X'); d2o += (tab[i][j] == 'O'); }
 		}
-	return false;
-}
-
-bool checkcol(int cc, char let) {
-	for(int i = 0; i < 3; i++)
-		if(c[i][cc] == 2 && !c[i][!cc]) { 
-			int j; for(j = 0; j < 3 && tab[j][i] != '#');
-			tab[j][i] = let;
-			return true;
+		if(p && cr == 3) return true;
+		if(!p && ot == 3) return true;	
+	}
+	if(p && (d1r == 3 || d2r == 3)) return true;
+	if(!p && (d1o == 3 || d2o == 3)) return true;
+	for(int j = 0; j < 3; j++) {
+		cr = ot = 0;
+		for(int i = 0; i < 3; i++) {
+			cr += tab[i][j] == 'X';
+			ot += tab[i][j] == 'O';
 		}
-	return false;
-}
-
-bool checkd1(int cc, char let) {
-	if(d1[cc] == 2 && !d1[!cc]) {
-		if(tab[0][0] == '#') tab[0][0] = let;
-		else if(tab[1][1] == '#') tab[1][1] = let;
-		else if(tab[2][2] == '#') tab[2][2] = let;
-		return true;
+		if(p && cr == 3) return true;
+		if(!p && ot == 3) return true;	
 	}
 	return false;
 }
 
-bool checkd2(int cc, char let) {
-	if(d2[cc] == 2 && !d2[!cc]) {
-		if(tab[0][2] == '#') tab[0][2] = let;
-		else if(tab[1][1] == '#') tab[1][1] = let;
-		else if(tab[2][0] == '#') tab[2][0] = let;
-		return true;
+int seen[3];
+vector<pii> pos;
+int beg;
+
+int play(int n, int j) {
+	if(n == 0) return 2;
+	char c = (j) ? 'X' : 'O';
+	int w, d, dr;
+	w = d = dr = 0;
+	for(int i = 0; i < 3; i++) {
+		int a = pos[i].fst, b = pos[i].snd;
+		if(seen[i]) continue;
+		seen[i] = 1; tab[a][b] = c;
+		if(check(j)) w++; 
+		else {
+			int ans = play(n-1, !j);
+			if(ans == 2) dr++;
+			else if(ans == j) w++;
+			else d++;
+		}
+		seen[i] = 0; tab[a][b] = '#';
 	}
-	return false;
+	if(w) return j;
+	if(dr) return 2;
+	return !j;
 }
 
 int main() {
 	for(int i = 0; i < 3; i++)
 		for(int j = 0; j < 3; j++) {
 			scanf(" %c", &tab[i][j]);
-			l[i][tab[i][j] == 'X']++;		
-			c[j][tab[i][j] == 'X']++;		
-			if(i == j) d1[tab[i][j] == 'X']++;
-			if(i+j == 2) d2[tab[i][j] == 'O']++; 
+			if(tab[i][j] == '#') pos.pb(pii(i, j));
 		}
-	if(checklin(1, 'X')) { puts("Crosses win"); return 0; }
-	if(checkcol(1, 'X')) { puts("Crosses win"); return 0; }
-	if(checkd1(1, 'X')) { puts("Crosses win"); return 0; }
-	if(checkd2(1, 'X')) { puts("Crosses win"); return 0; }
-	if(!checklin(0, 'X') && !checkcol(0, 'X') && !checkd1(0, 'X') && !checkd2(0, 'X')) {
-		puts("Draw"); return 0;
-	}
-
-	return 0;
-}
-
-		
-	
-		
-	
+	int ans = play(3, 1);
+	if(ans == 1) puts("Crosses win");
+	else if(ans == 0) puts("Ouths win");
+	else puts("Draw");
 	return 0;	
 }
