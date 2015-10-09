18c126ea61656b7a3777fa06881c2347e99b93f7
gidelfino <giovana.delfino@gmail.com>
2015-10-08 22:35:19 -0300

    e ceus

--- Gi/icpc/1411.cpp
+++ Gi/icpc/1411.cpp
@@ -12,45 +12,38 @@ const ll modn = 1000000007;
 inline ll mod(ll x) { return x % modn; }
 
 int tab[55][55];
-int mark[55][55];
 
 int main() {
 	for_tests(t, tt) {
 		int n, r, x, y, l, c, i, j, dir;
 		scanf("%d %d", &n, &r);
 		memset(tab, 0, sizeof tab);
-		memset(mark, 0, sizeof mark);
 		for(i = 0; i < r; i++)
-			scanf("%d %d", &x, &y), tab[x][y] = 1;
+			scanf("%d %d", &x, &y), tab[y][x] = 1;
 		scanf("%d %d", &x, &y);
 		if(x == 0 || x > n) { dir = 1 + (x == 0); l = y; c = x; }  
 		if(y == 0 || y > n) { dir = 3 + (y == 0); c = x; l = y; }  
 		while(42) {
 			if(dir == 1) {
 				for(j = c-1; j >= 1 && !tab[l][j]; j--);
-				if(mark[l][j]) { puts("0 0"); break; }
 				if(j == 0) { printf("%d %d\n", j, l); break; }
-				mark[l][j] = 1; dir = 3; c = j;
+				dir = 4; c = j;
 			}
 			else if(dir == 2) {
 				for(j = c+1; j <= n && !tab[l][j]; j++);
-				if(mark[l][j]) { puts("0 0"); break; }
 				if(j > n) { printf("%d %d\n", j, l); break; }
-				mark[l][j] = 1; dir = 4; c = j;
+				dir = 3; c = j;
 			}
 			else if(dir == 3) {
 				for(i = l-1; i >= 1 && !tab[i][c]; i--);
-				if(mark[i][c]) { puts("0 0"); break; }
 				if(i == 0) { printf("%d %d\n", c, i); break; }
-				mark[i][c] = 1; dir = 2; l = i;
+				dir = 1; l = i;
 			}
 			else if(dir == 4) {
 				for(i = l+1; i <= n && !tab[i][c]; i++);
-				if(mark[i][c]) { puts("0 0"); break; }
 				if(i > n) { printf("%d %d\n", c, i); break; }
-				mark[i][c] = 1; dir = 1; l = i;
+				dir = 2; l = i;
 			}
-		//	printf("Next dir %d pos %d %d\n", dir, l, c);	
 		}
 	}
 	return 0;
