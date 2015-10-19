a66414b9d93e0a9101f2c5645e6faca9c6963d64
Yan Couto <yancouto@gmail.com>
2015-10-19 11:32:01 -0200

    2219 incompleto

--- Yan/icpc/2219.cpp
+++ Yan/icpc/2219.cpp
@@ -1,80 +1,65 @@
-// WAAA
 #include <bits/stdc++.h>
 using namespace std;
 #define fst first
 #define snd second
-typedef pair<int, int> pii;
 typedef unsigned long long ull;
 typedef long long ll;
-typedef long double ld;
+typedef pair<int, int> pii;
 #define pb push_back
 #define for_tests(t, tt) int t; scanf("%d", &t); for(int tt = 1; tt <= t; tt++)
 template<typename T> inline T abs(T t) { return t < 0? -t : t; }
-#ifndef ONLINE_JUDGE
-#	define debug(args...) {fprintf(stderr, "%3d| ", __LINE__); fprintf(stderr, args); fprintf(stderr, "\n");}
-#else
-#	define debug(args...) {}
-#endif
-const ull modn = 1000000007;
-inline ull mod(ull x) { return x % modn; }
-int n, c;
-vector<pii> adj[103];
-int deg[103], br[103];
-int c1[503], c2[503];
-int seen[103];
-void dfs(int u) {
-	if(seen[u]) return;
-	seen[u] = 1;
-	for(pii v : adj[u])
-		dfs(v.fst);
+const ll modn = 1000000007;
+inline ll mod(ll x) { return x % modn; }
+vector<int> cit[502], adj[502];
+
+int S[502], sz[502];
+int find(int i) {
+	if(S[S[i]] != S[i]) S[i] = find(S[i]);
+	return S[i];
+}
+void join(int a, int b) {
+	if((a = find(a)) == (b = find(b))) return;
+	if(sz[a] < sz[b]) swap(a, b);
+	sz[a] += sz[b];
+	S[b] = a;
 }
 
 int main() {
-	int k, a, i, j;
+	int n, c, i, k, j, x;
 	while(true) {
 		scanf("%d %d", &n, &c);
-		if(!n) return 0;
-		for(i = 0; i < c; i++) {
-			adj[i].clear();
-			seen[i] = 0;
-		}
-		memset(c1, -1, sizeof c1);
-		memset(c2, -1, sizeof c2);
+		for(i = 0; i < n; i++)
+			cit[i].clear();
+		int rc = c;
 		for(i = 0; i < c; i++) {
 			scanf("%d", &k);
-			br[i] = k;
+			S[i] = i; sz[i] = 1;
+			adj[i].clear();
+			if(!k) rc--;
 			for(j = 0; j < k; j++) {
-				scanf("%d", &a);
-				if(c1[a] != -1) c2[a] = i;
-				else c1[a] = i;
+				scanf("%d", &x);
+				cit[x].pb(i);
 			}
 		}
-		for(i = 0; i < n; i++) {
-			if(c2[i] == -1) c2[i] = c1[i];
-			adj[c2[i]].pb(pii(c1[i], i));
-			adj[c1[i]].pb(pii(c2[i], i));
+		for(i = 0; i < n; i++)  {
+			join(cit[i].front(), cit[i].back());
+			adj[cit[i].front()].pb(cit[i].back());
+			if(cit[i].size() > 1) adj[cit[i].back()].pb(cit[i].front());
 		}
+		int odd = 0;
 		for(i = 0; i < c; i++)
-			if(br[i]) {
-				dfs(i);
-				break;
-			}
-		for(i = 0; i < c; i++)
-			if(!seen[i] && br[i])
-				break;
-		if(i < c) { puts("-1"); continue; }
-		vector<int> imp;
-		for(i = 0; i < c; i++)
-			if(adj[i].size() & 1)
-				imp.pb(i);
-		if(imp.size() > 2) puts("-1");
-		else if(imp.empty()) puts("0");
-		else {
-			int mn = INT_MAX;
-			for(int u : imp)
-				for(pii v : adj[u])
-					mn = min(v.snd, mn);
-			printf("%d\n", mn);
+			odd += adj[i].size() & 1;
+		if((odd != 0 && odd != 2) || sz[find(cit[0].back())] != rc) {
+			puts("-1");
+			continue;
 		}
+		if(odd == 0) {
+			puts("0");
+			continue;
+		}
+		for(i = 0; i < n; i++)
+			if((adj[cit[i].front()].size() & 1) || (adj[cit[i].back()].size() & 1))
+				break;
+		printf("%d\n", i);
 	}
 }
