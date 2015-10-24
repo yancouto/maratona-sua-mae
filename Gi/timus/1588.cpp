722c5477873014899f6b1fc28f248e8adc4bcd20
gidelfino <giovana.delfino@gmail.com>
2015-10-23 22:56:52 -0200

    foi perceber que

--- Gi/timus/1588.cpp
+++ Gi/timus/1588.cpp
@@ -31,38 +31,24 @@ int main() {
 			ans += dist(pt[i], pt[j]);
 	for(i = 0; i < n; i++) {
 		for(j = i + 1; j < n; j++) {
-			sl1 = sl2 = 0;
+			sl1 = sl2 = sl3 = 0;
 			if(seen[i][j]) continue;
-			seen[i][j] = 1;
+			seen[i][j] = 1; lst = j;
 			if(pt[j].x - pt[i].x)
 				sl1 = (double) (pt[j].y - pt[i].y) / (pt[j].x - pt[i].x);
-			lst = j;
 			for(k = j + 1; k < n; k++) {
-				//putchar('\n');	
-				//puts("----------------------------------");
 				if(pt[k].x - pt[i].x)
 					sl2 = (double) (pt[k].y - pt[i].y) / (pt[k].x - pt[i].x);
 				if(pt[k].x - pt[j].x)
 					sl3 = (double) (pt[k].y - pt[j].y) / (pt[k].x - pt[j].x);
-				//printf("sls %lf %lf\n", sl1, sl2);
 				if(!check(sl1, sl2) || !check(sl2, sl3)) continue;
-	//			printf("Pontos (%d, %d) (%d, %d) (%d, %d)\n", pt[i].x, pt[i].y, pt[j].x, pt[j].y, pt[k].x, pt[k].y);
-	//			printf("*************Na mesma linha***************\n");
-	//			printf("1Tirando distancia (%d %d) (%d %d)\n", pt[lst].x, pt[lst].y, pt[i].x, pt[i].y);
 				ans -= dist(pt[lst], pt[i]);
 				lst = k; seen[i][k] = 1;
-				//printf("dist (%d %d) (%d %d) %lf\n", pt[k].x, pt[k].y, lst.x, lst.y, dist(pt[k], lst)); 
-				//printf("2SOMANDO DISTS (%d %d) (%d %d) = %lf\n", pt[j].x, pt[j].y, pt[k].x, pt[k].y, dist(pt[k], pt[j]));
 			}
-			if(lst != j)
-	//			printf("2Tirando distancia (%d %d) (%d %d)\n", pt[lst].x, pt[lst].y, pt[j].x, pt[j].y);
 			if(lst != j) ans -= dist(pt[j], pt[lst]);
 		}
 	}
-				//printf("Pontos (%d, %d) (%d, %d) (%d, %d)\n", pt[i].x, pt[i].y, pt[j].x, pt[j].y, pt[k].x, pt[k].y);
-				//printf("sls %lf %lf\n", sl1, sl2);
 	printf("%01.f\n", floor(ans + 0.5));	
-
 	return 0;
 }
 
