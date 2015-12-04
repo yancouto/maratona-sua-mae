db85f1d1269de78d93334ea588995a0ad6744ffa
Yan Couto <yancouto@gmail.com>
2015-12-04 13:56:18 -0200

    qtree + desafios

--- Yan/spoj/qtree.cpp
+++ Yan/spoj/qtree.cpp
@@ -1,4 +1,3 @@
-// RE??
 #include <bits/stdc++.h>
 using namespace std;
 #define fst first
@@ -12,25 +11,20 @@ template<typename T> inline T abs(T t) { return t < 0? -t : t; }
 const ll modn = 1000000007;
 inline ll mod(ll x) { return x % modn; }
 
-const int N = 10009;
-vector<pii> adj[N];
-int val[N], c[N], nv[N], pai[N][15], so[N];
-int inv[N], cost[N];
-int dfs(int u, int p, int c, int nv) {
-	int &f = so[u];
+int dfs(int u, int p, int nv) {
+	int i; int &f = vs[u];
 	::nv[u] = nv;
 	pai[u][0] = p;
-	for(int i = 1; i < 15; i++)
+	for(i = 1; i < 15; i++)
 		pai[u][i] = pai[pai[u][i-1]][i-1];
-	if(p != u)  {
-		for(f = 0; adj[u][f].fst != p; f++);
-		adj[u].erase(adj[u].begin() + f);
-		val[u] = c; inv[c] = u;
+	if(p != u) {
+		for(i = 0; adj[u][i].fst != p; i++);
+		ev[adj[u][i].snd] = u;
+		cost[u] = c[adj[u][i].snd];
+		adj[u].erase(adj[u].begin() + i);
 	}
-	f = 1;
-	cost[u] = ::c[c];
-	for(pii v : adj[u])
-		f += dfs(v.fst, u, v.snd, nv + 1);
+	for(pii p : adj[u])
+		f += dfs(p.fst, u, nv + 1);
 	return f;
 }
 
@@ -45,10 +39,9 @@ int lca(int u, int v) {
 			u = pai[u][i], v = pai[v][i];
 	return pai[u][0];
 }
-
-int cur, ci[N], cp[N], cs[N], pr[N], th[N], tr[N << 4], tc, L[N << 4], R[N << 4], ch[N];
+int cs[N], ch[N], ct[N], tr[N << 2], L[N << 2], R[N << 2], tc, cur, vc[N], vp[N], pr[N];
 void build(int i, int l, int r) {
-	if(l == r) { tr[i] = (!!l) * pr[l]; return; }
+	if(l == r) return (void) tr[i] = pr[l];
 	int m = (l + r) / 2;
 	build(L[i] = tc++, l, m);
 	build(R[i] = tc++, m + 1, r);
@@ -62,74 +55,64 @@ int get(int i, int l, int r, int ql, int qr) {
 	return max(get(L[i], l, m, ql, qr), get(R[i], m + 1, r, ql, qr));
 }
 
-void set_tree(int i, int l, int r, int x, int val) {
-	if(l == r) { tr[i] = val; return; }
+void set_t(int i, int l, int r, int p, int x) {
+	if(l == r) return (void) tr[i] = x;
 	int m = (l + r) / 2;
-	if(x <= m) set_tree(L[i], l, m, x, val);
-	else set_tree(R[i], m + 1, r, x, val);
-	tr[i] = max(tr[L[i]], tr[R[i]]);
+	if(p <= m) set_t(L[i], l, m, p, x);
+	else set_t(R[i], m + 1, r, p, x);
 }
 
 void hld(int u) {
-	pr[cs[cur]] = cost[u];
-	cp[u] = cs[cur];
-	if(!cs[cur]) ch[cur] = u;
-	cs[cur]++;
-	ci[u] = cur;
-	if(adj[u].empty()) return build(th[cur] = tc++, 0, cs[cur] - 1);
-	int bi = adj[u].front().fst;
+	vc[u] = cur;
+	vp[u] = cs[cur]++;
+	pr[vp[u]] = (!!vp[u]) * cost[u];
+	if(adj[u].empty()) return build(ct[cur] = tc++, 0, cs[cur] - 1);
+	int bi = 0;
 	for(int i = 1; i < adj[u].size(); i++)
-		if(so[adj[u][i].fst] > so[bi])
-			bi = adj[u][i].fst;
+		if(so[adj[u][i].fst] > so[adj[u][bi].fst])
+			bi = i;
 	hld(bi);
-	for(pii v : adj[u])
-		if(v.fst != bi) {
+	for(pii p : adj[u])
+		if(p.fst != bi) {
 			cur++;
-			hld(v.fst);
+			hld(p.fst);
 		}
 }
 
-int mx(int a, int b) {
-	int x = INT_MIN;
+int path(int a, int b) {
+	int mx = INT_MIN;
 	while(a != b) {
 		if(cp[a]) {
-			int d = 0;
-			if(ci[a] == ci[b]) d = cp[b] + 1;
-			x = max(x, get(th[ci[a]], 0, cs[ci[a]] - 1, d, cp[a]));
-			a = ch[ci[a]];
-			if(ci[a] == ci[b]) a = b;
+			int c = vc[a]; int d = ch[c];
 		} else {
-			x = max(x, cost[a]);
+			mx = max(mx, cost[a]);
 			a = pai[a][0];
 		}
 	}
-	return x;
+	return mx;
 }
 
+
 int main() {
-	int i, n, a, b; char c;
 	for_tests(t, tt) {
 		scanf("%d", &n);
-		for(i = 0; i < n; i++)
-			adj[i].clear();
+		for(i = 0; i < n; i++) adj[i].clear(), cs[i] = 0, vs[i] = 1;
 		for(i = 0; i < n - 1; i++) {
-			scanf("%d %d %d", &a, &b, &::c[i]); a--; b--;
+			scanf("%d %d %d", &a, &b, &c[i]); a--; b--;
 			adj[a].pb(pii(b, i));
 			adj[b].pb(pii(a, i));
 		}
-		dfs(0, 0, 0, 0);
-		memset(cs, 0, sizeof cs);
-		cur = tc = 0;
+		dfs(0, 0, 0);
+		tc = cur = 0;
 		hld(0);
 		while(scanf(" %c%*s %d %d", &c, &a, &b) == 3) {
 			if(c == 'C') {
-				int u = inv[--a];
-				if(cp[u]) set_tree(th[ci[u]], 0, cs[ci[u]] - 1, cp[u], b);
-				else cost[u] = b;
+				int v = ev[--a]; int ch = vc[v];
+				if(cp[v]) set_t(ct[ch], 0, cs[ch] - 1, vp[v], b);
+				else cost[v] = b;
 			} else {
-				a--; b--;
-				int l = lca(a, b);
-				printf("%d\n", max(mx(a, l), mx(b, l)));
+				int c = lca(--a, --b);
+				printf("%d\n", max(path(a, c), path(b, c)));
 			}
 		}
 	}
