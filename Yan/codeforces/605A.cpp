2af5b471787f85c1b1ade7e32a60fa7eae40f4e4
Yan Couto <yancouto@gmail.com>
2015-12-12 20:43:42 -0200

    convex hull trick master + hld fail + cfs + kattis

--- Yan/codeforces/605A.cpp
+++ Yan/codeforces/605A.cpp
@@ -1,4 +1,3 @@
-
 #include <bits/stdc++.h>
 using namespace std;
 #define fst first
@@ -11,3 +10,21 @@ typedef pair<int, int> pii;
 template<typename T> inline T abs(T t) { return t < 0? -t : t; }
 const ll modn = 1000000007;
 inline ll mod(ll x) { return x % modn; }
+
+int a[100009], pos[100009];
+int main() {
+	int i, j, n;
+	scanf("%d", &n);
+	for(i = 0; i < n; i++) {
+		scanf("%d", &a[i]);
+		pos[a[i]] = i;
+	}
+	int cur = pos[1], mx = 1, c = 1;
+	for(i = 2; i <= n; i++) {
+		if(pos[i] > cur) c++;
+		else c = 1;
+		cur = pos[i];
+		mx = max(mx, c);
+	}
+	printf("%d\n", n - mx);
+}
--- Yan/spoj/qtree2.cpp
+++ Yan/spoj/qtree2.cpp
@@ -1,3 +1,4 @@
+//RE
 #include <bits/stdc++.h>
 using namespace std;
 #define fst first
@@ -10,26 +11,20 @@ typedef pair<int, int> pii;
 template<typename T> inline T abs(T t) { return t < 0? -t : t; }
 const ll modn = 1000000007;
 inline ll mod(ll x) { return x % modn; }
-const int N = 11000;
-vector<pii> adj[N];
-int nv[N], pai[N][15], vs[N], c[N];
-int vp[N], vc[N], ev[N], ct[N], cs[N], ch[N], cost[N], cur, pv[N];
-int tr[N << 2], L[N << 2], R[N << 2], tn;
-
+const int N = 11009;
+vector<pii> adj[N]; int so[N], pai[N][15], cost[N], c[N], ev[N], nv[N];
 int dfs(int u, int p, int nv) {
-	int i;
+	int i; int &f = so[u];
 	::nv[u] = nv;
 	pai[u][0] = p;
 	for(i = 1; i < 15; i++)
-		pai[u][i] = pai[pai[u][i-1]][i-1];
+		pai[u][i] = pai[pai[u][i - 1]][i - 1];
 	if(u != p) {
 		for(i = 0; adj[u][i].fst != p; i++);
 		ev[adj[u][i].snd] = u;
 		cost[u] = c[adj[u][i].snd];
-		swap(adj[u][i], adj[u].back());
-		adj[u].pop_back();
+		adj[u].erase(adj[u].begin() + i);
 	}
-	int &f = vs[u];
 	for(pii p : adj[u])
 		f += dfs(p.fst, u, nv + 1);
 	return f;
@@ -47,39 +42,44 @@ int lca(int u, int v) {
 	return pai[u][0];
 }
 
-void build(int i, int l, int r) {
-	if(l == r) return (void) (tr[i] = pv[l]);
+int tr[N << 2], val[N];
+inline int L(int i) { return i << 1; }
+inline int R(int i) { return (i << 1) + 1; }
+void build(int *tr, int i, int l, int r) {
+	if(l == r) return (void) (tr[i] = val[l]);
 	int m = (l + r) / 2;
-	build(L[i] = tn++, l, m);
-	build(R[i] = tn++, m + 1, r);
-	tr[i] = max(tr[L[i]], tr[R[i]]);
+	build(tr, L(i), l, m);
+	build(tr, R(i), m + 1, r);
+	tr[i] = max(tr[L(i)], tr[R(i)]);
 }
 
-int query(int i, int l, int r, int ql, int qr) {
+int query(int *tr, int i, int l, int r, int ql, int qr) {
 	if(l > qr || r < ql) return INT_MIN;
 	if(l >= ql && r <= qr) return tr[i];
 	int m = (l + r) / 2;
-	return max(query(L[i], l, m, ql, qr), query(R[i], m + 1, r, ql, qr));
+	return max(query(tr, L(i), l, m, ql, qr), query(tr, R(i), m + 1, r, ql, qr));
 }
 
-void change(int i, int l, int r, int pos, int x) {
-	if(l == r) return (void) (tr[i] = x);
+void update(int *tr, int i, int l, int r, int x, int val) {
+	if(l == r) return (void) (tr[i] = val);
 	int m = (l + r) / 2;
-	if(pos <= m) change(L[i], l, m, pos, x);
-	else change(R[i], m + 1, r, pos, x);
-	tr[i] = max(tr[L[i]], tr[R[i]]);
+	if(x <= m) update(tr, L(i), l, m, x, val);
+	else update(tr, R(i), m + 1, r, x, val);
+	tr[i] = max(tr[L(i)], tr[R(i)]);
 }
+
+int cur, tot, vc[N], vp[N], ch[N], cs[N];
+int *ct[N];
 
 void hld(int u) {
-	vp[u] = cs[cur]++;
-	if(vp[u] == 0) ch[cur] = u;
+	tot++;
 	vc[u] = cur;
-	pv[vp[u]] = cost[u];
-	if(adj[u].empty() && cs[cur] > 1) return build(ct[cur] = tn++, 1, cs[cur] - 1);
-	if(adj[u].empty()) return;
+	val[vp[u] = cs[cur]++] = cost[u];
+	if(vp[u] == 0) ch[cur] = u;
+	if(adj[u].empty()) return build(ct[cur] = tr + (tot - cs[cur]) * 4 - 1, 1, 0, cs[cur] - 1);
 	int bi = adj[u].front().fst;
 	for(pii p : adj[u])
-		if(vs[p.fst] > vs[bi])
+		if(so[p.fst] > so[bi])
 			bi = p.fst;
 	hld(bi);
 	for(pii p : adj[u])
@@ -89,18 +89,13 @@ void hld(int u) {
 		}
 }
 
-int path(int a, int b) {
-	int mx = INT_MIN;
-	while(a != b) {
-		if(vp[a]) {
-			int v = ch[vc[a]];
-			if(vc[a] == vc[b]) v = b;
-			mx = max(mx, query(ct[vc[a]], 1, cs[vc[a]] - 1, vp[v] + 1, vp[a]));
-			a = v;
-		} else {
-			mx = max(mx, cost[a]);
-			a = pai[a][0];
-		}
+int path(int u, int v) {
+	int mx = 0;
+	while(u != v) {
+		int c = vc[u];
+		if(c == vc[v]) return max(mx, query(ct[c], 1, 0, cs[c] - 1, vp[v] + 1, vp[u]));
+		mx = max(mx, query(ct[c], 1, 0, cs[c] - 1, 0, vp[u]));
+		u = pai[ch[c]][0];
 	}
 	return mx;
 }
@@ -110,27 +105,23 @@ int main() {
 	int i, n, a, b;
 	for_tests(t, tt) {
 		scanf("%d", &n);
-		for(i = 0; i < n; i++)
-			adj[i].clear(), cs[i] = 0, vs[i] = 1;
+		for(i = 0; i < n; i++) cs[i] = 0, adj[i].clear(), so[i] = 1;
 		for(i = 0; i < n - 1; i++) {
 			scanf("%d %d %d", &a, &b, &c[i]); a--; b--;
 			adj[a].pb(pii(b, i));
 			adj[b].pb(pii(a, i));
 		}
-		tn = cur = 0;
 		dfs(0, 0, 0);
+		tot = cur = 0;
 		hld(0);
 		while(true) {
 			scanf("%s", s);
 			if(s[0] == 'D') break;
 			scanf("%d %d", &a, &b);
-			if(s[0] == 'C') {
-				int v = ev[--a]; int c = vc[v];
-				if(vp[v]) change(ct[c], 1, cs[c] - 1, vp[v], b);
-				else cost[v] = b;
-			} else {
-				int c = lca(--a, --b);
-				printf("%d\n", max(path(a, c), path(b, c)));
+			if(s[0] == 'C') update(ct[vc[ev[a-1]]], 1, 0, cs[vc[ev[a-1]]] - 1, vp[ev[a-1]], b);
+			else {
+				int l = lca(a - 1, b - 1);
+				printf("%d\n", max(path(a - 1, l), path(b - 1, l)));
 			}
 		}
 	}
