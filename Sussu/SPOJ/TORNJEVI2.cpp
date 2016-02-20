b7883a1c3c86928ab248592eac5e546fc6173a8b
gafeol <gabfeol@gmail.com>
2016-02-20 00:13:21 -0200

    avancos tornjevi

--- Sussu/SPOJ/TORNJEVI2.cpp
+++ Sussu/SPOJ/TORNJEVI2.cpp
@@ -16,8 +16,9 @@ const int MAXN = 110;
 int n, m, deg;
 
 char M[MAXN][MAXN];
-int ind[MAXN][MAXN], mrk[MAXN*MAXN*4], c[MAXN*MAXN*4];
-
+int ind[MAXN][MAXN], mrk[MAXN*MAXN*4], c[MAXN*MAXN*4], proi[MAXN*MAXN*4], degr[MAXN*MAXN*4];
+int jah[MAXN*MAXN], jav[MAXN*MAXN], res[MAXN*MAXN];
+int resv[MAXN], resh[MAXN], ja[MAXN*MAXN*4];
 int U[MAXN][MAXN]; 
 int D[MAXN][MAXN]; 
 int R[MAXN][MAXN];
@@ -25,7 +26,11 @@ int L[MAXN][MAXN];
 
 pii tow[MAXN*MAXN];
 
+queue <int> topsrt;
+
 vector <int> adj[MAXN*MAXN*4], inv[MAXN*MAXN*4];
+
+vector <int> vc[MAXN*MAXN*4];
 
 stack <int> s;
 
@@ -47,6 +52,7 @@ int nao(int v){
 
 void add(int u, int v){
 	adj[u].pb(v);
+	degr[u]++;
 	inv[v].pb(u);
 }
 
@@ -60,8 +66,9 @@ void dfs(int v){
 }
 
 void go(int v){
+	vc[c[v]].pb(v);
 	for(int nxt: inv[v]){
-		if(!c[nxt]) continue;
+		if(c[nxt]) continue;
 		c[nxt] = c[v];
 		go(nxt);
 	}
@@ -124,74 +131,86 @@ int main (){
 					printf("add(nao(hor(%d)), sim(hor(%d)))\n", r, r);
 					add(sim(hor(l)), nao(hor(l)));
 					printf("add(sim(hor(%d)), nao(hor(%d)))\n", l, l);
+					proi[nao(hor(r))] = 1;
+					proi[sim(hor(l))] = 1;
 				}
 				else if( d == 0 ){
 					add(sim(ver(u)), nao(ver(u)));
-					printf("add(nao(ver(%d)), sim(ver(%d)))\n", d, d);
+					printf("add(sim(ver(%d)), nao(ver(%d)))\n", u, u);
 					add(nao(hor(r)), sim(hor(r)));
-					printf("add(nao(ver(%d)), sim(ver(%d)))\n", d, d);
+					printf("add(nao(hor(%d)), sim(hor(%d)))\n", r, r);
 					add(sim(hor(l)), nao(hor(l)));
-					printf("add(nao(ver(%d)), sim(ver(%d)))\n", d, d);
+					printf("add(sim(hor(%d)), nao(hor(%d)))\n", l, l);
+					proi[nao(hor(r))] = 1;
+					proi[sim(hor(l))] = 1;
 				}
 				else if( l == 0 ){
 					add(sim(hor(r)), nao(hor(r)));
-					printf("add(nao(ver(%d)), sim(ver(%d)))\n", d, d);
+					printf("add(sim(hor(%d)), nao(hor(%d)))\n", r, r);
 					add(nao(ver(u)), sim(ver(u)));
-					printf("add(nao(ver(%d)), sim(ver(%d)))\n", d, d);
+					printf("add(nao(ver(%d)), sim(ver(%d)))\n", u, u);
 					add(sim(ver(d)), nao(ver(d)));
-					printf("add(nao(ver(%d)), sim(ver(%d)))\n", d, d);
+					printf("add(sim(ver(%d)), nao(ver(%d)))\n", d, d);
+					proi[nao(ver(u))] = 1;
+					proi[sim(ver(d))] = 1;
 				}
 				else{
 					add(nao(hor(l)), sim(hor(l)));
-					printf("add(nao(ver(%d)), sim(ver(%d)))\n", d, d);
+					printf("add(nao(hor(%d)), sim(hor(%d)))\n", l, l);
 					add(nao(ver(u)), sim(ver(u)));
-					printf("add(nao(ver(%d)), sim(ver(%d)))\n", d, d);
+					printf("add(nao(ver(%d)), sim(ver(%d)))\n", u, u);
 					add(sim(ver(d)), nao(ver(d)));
-					printf("add(nao(ver(%d)), sim(ver(%d)))\n", d, d);
+					printf("add(sim(ver(%d)), nao(ver(%d)))\n", d, d);
+					proi[nao(ver(u))] = 1;
+					proi[sim(ver(d))] = 1;
 				}
 			}
 			else if((u != 0) + (d != 0) + (l != 0) + (r != 0) == 2){
 				if( u + r == 0 ){
 					add(nao(hor(l)), sim(ver(d)));
-					printf("add(nao(ver(%d)), sim(ver(%d)))\n", d, d);
+					printf("add(nao(hor(%d)), sim(ver(%d)))\n", l, d);
 					add(nao(ver(d)), sim(hor(l)));
-					printf("add(nao(ver(%d)), sim(ver(%d)))\n", d, d);
+					printf("add(nao(ver(%d)), sim(hor(%d)))\n", d, l);
 				}
 				else if( u + l == 0 ){
 					add(sim(hor(r)), sim(ver(d)));
-					printf("add(nao(ver(%d)), sim(ver(%d)))\n", d, d);
+					printf("add(sim(hor(%d)), sim(ver(%d)))\n", r, d);
 					add(nao(ver(d)), nao(hor(r)));
-					printf("add(nao(ver(%d)), sim(ver(%d)))\n", d, d);
+					printf("add(nao(ver(%d)), nao(hor(%d)))\n", d, r);
 				}
 				else if( d + r == 0 ){
 					add(nao(hor(l)), nao(ver(u)));
-					printf("add(nao(ver(%d)), sim(ver(%d)))\n", d, d);
+					printf("add(nao(hor(%d)), nao(ver(%d)))\n", d, d);
 					add(sim(ver(u)), sim(hor(l)));
-					printf("add(nao(ver(%d)), sim(ver(%d)))\n", d, d);
+					printf("dd(sim(ver(%d)), sim(hor(%d)))\n", u, l);
 				}
 				else if( d + l == 0){
 					add(sim(hor(r)), nao(ver(u)));
-					printf("add(nao(ver(%d)), sim(ver(%d)))\n", d, d);
+					printf("add(sim(hor(%d)), nao(ver(%d)))\n", r, u);
 					add(sim(ver(u)), nao(hor(r)));
-					printf("add(nao(ver(%d)), sim(ver(%d)))\n", d, d);
+					printf("add(sim(ver(%d)), nao(hor(%d)));\n", u, r);
 				}
 			}
 			else{
 				if( u != 0 ){
 					add(sim(ver(u)), nao(ver(u)));
-					printf("add(nao(ver(%d)), sim(ver(%d)))\n", d, d);
+					printf("add(sim(ver(%d)), nao(ver(%d)))\n", u, u);
+					proi[sim(ver(u))] = 1;
 				}
 				else if( d != 0 ){
 					add(nao(ver(d)), sim(ver(d)));
 					printf("add(nao(ver(%d)), sim(ver(%d)))\n", d, d);
+					proi[nao(ver(d))] = 1;
 				}
 				else if( l != 0 ){
 					add(nao(hor(l)), sim(hor(l)));
-					printf("add(nao(ver(%d)), sim(ver(%d)))\n", d, d);
+					printf("add(nao(hor(%d)), sim(hor(%d)))\n", l, l);
+					proi[nao(hor(l))] = 1;
 				}
 				else{
 					add(sim(hor(r)), nao(hor(r)));
-					printf("add(nao(ver(%d)), sim(ver(%d)))\n", d, d);
+					printf("add(sim(hor(r)), nao(hor(r)))\n", r, r);
+					proi[sim(hor(r))] = 1;
 				}
 			}
 		}
@@ -205,14 +224,88 @@ int main (){
 	while(!s.empty()){
 		int v = s.top();
 		s.pop();
+		if(c[v]) continue;
 		c[v] = cor++;
 		go(v);
 	}
+
+	/*
 	for(int a=ver(1);a <= hor(deg-1);a++){
 		if(c[sim(a)] == c[nao(a)]){
-			printf("FUDEU %d - ind %d\n", a, a/2);
+			printf("FUDEU %d - ind %d de cor %d\n", a, a/2, c[sim(a)]);
 			return 0;
 		}
 	}
-	printf("funfou\n");
+	for(int a=1;a<cor;a++){
+		int ver = 0;
+		for(int v: vc[a]){
+			if(((v/2)&1 && jah[v/4]) || (!((v/2)&1) && jav[v/4]) || proi[v]) {ver = 1; break;}
+		}
+		if(!ver){
+			for(int v: vc[a]){
+				if(((v/2)&1)){
+					jah[v/4] = 1;
+					res[v/4] += (v%4 - 1);
+				}
+				else{
+					jav[v/4] = 1;
+					if(!(v&1)) res[v/4] += 2;
+				}
+
+			}
+		}
+	}
+	for(int a=1;a<=n;a++){
+		for(int b=1;b<=m;b++){
+			if(M[a][b] != 'T') printf("%c", M[a][b]);
+			else{
+				if(res[ind[a][b]] == 1 || res[ind[a][b]] == 2)
+					res[ind[a][b]] = 3 - res[ind[a][b]];
+				printf("%d", res[ind[a][b]]);
+			}
+		}
+		printf("\n");
+	}
+	*/
+	for(int a=sim(ver(1));a<=nao(hor(deg - 1));a++){
+		if(degr[a] == 0) topsrt.push(a);
+	}
+	while(!topsrt.empty()){
+		int v = topsrt.front();
+		topsrt.pop();
+		for(int nxt: adj[v]){
+			degr[nxt]--;
+			if(degr[nxt] == 0)
+				topsrt.push(nxt);
+		}
+		if(ja[v]) continue;
+		if(v&1){ //hor
+			resh[v/4] = ((v/2)&1);
+			ja[v] = 1;
+			if(v%4 < 2)
+				ja[v+2] = 1;
+			else
+				ja[v-2] = 1;
+		}
+		else{
+			resv[v/4] = ((v/2)&1);
+			ja[v] = 1;
+			if(v%4 < 2)
+				ja[v+2] = 1;
+			else
+				ja[v-2] = 1;
+		}
+	}
+	for(int a=1;a<=deg-1;a++){
+		if(!resh[a] && !resv[a]) M[tow[a].fst][tow[a].snd] = '1';
+		if(resh[a] && !resv[a]) M[tow[a].fst][tow[a].snd] = '2';
+		if(resh[a] && resv[a]) M[tow[a].fst][tow[a].snd] = '3';
+		if(!resh[a] && resv[a]) M[tow[a].fst][tow[a].snd] = '4';
+	}
+	for(int a=1;a<=n;a++){
+		for(int b=1;b<=m;b++){
+			printf("%c", M[a][b]);
+		}
+		printf("\n");
+	}
 }
