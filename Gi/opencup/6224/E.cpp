f213eec326c4653f5ec80b17b2014312597c108d
gidelfino <giovana.delfino@gmail.com>
2016-02-25 00:44:51 -0300

    Nelson nao conseguia respirar

--- Gi/opencup/6224/E.cpp
+++ Gi/opencup/6224/E.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define fst first
-#define snd second
+#define x first
+#define y second
 typedef unsigned long long ull;
 typedef long long ll;
 typedef pair<ll, ll> pii;
@@ -10,17 +10,35 @@ typedef pair<ll, ll> pii;
 template<typename T> inline T abs(T t) { return t < 0? -t : t; }
 const ll modn = 1000000007;
 inline ll mod(ll x) { return x % modn; }
-const ll oo = 10000000000;
+const ll oo = 1000000000000000;
 const int D = 8, dd = 30;
 
-ll X, Y, tmp;
+ll X, Y;
 ll dx[D] = {1,-1, 1,-1,2,-2, 2,-2};
 ll dy[D] = {2, 2,-2,-2,1, 1,-1,-1};
-ll seen[100][100], memo[100][100];
+ll best[100][100];
+
+bool isIn(ll x, ll y) { return x <= dd && x >= -dd && y <= dd && y >= -dd; }
+		
+void bfs() {
+	deque<pii> dq;
+	dq.pb(pii(0, 0));
+	best[0+dd][0+dd] = 0;
+	while(!dq.empty()) {
+		pii v = dq.front(); dq.pop_front();
+		for(int i = 0; i < D; i++) {
+			if(!isIn(v.x+dx[i], v.y+dy[i]) || best[v.x+dx[i]+dd][v.y+dy[i]+dd] != -1) continue;
+			best[v.x+dx[i]+dd][v.y+dy[i]+dd] = best[v.x+dd][v.y+dd] + 1ll;
+			dq.pb(pii(v.x+dx[i], v.y+dy[i]));
+		}
+	}
+}
 
 ll dist(ll x,ll y) {
 	int i, j;
 	ll ret = oo;
+	x = X - x; y = Y - y;
+	if(!x && !y) return 0;
 	for(i = 0; i < D; i++)
 		for(j = i + 1; j < D; j++) {
 			ll A = dy[j] * dx[i] - dy[i] * dx[j], B = y * dx[i] - dy[i] * x;
@@ -34,23 +52,25 @@ ll dist(ll x,ll y) {
 	return ret;
 }
 
-bool isIn(ll x, ll y) { return x <= X + dd && x >= X - dd && y <= Y + dd && y >= Y - dd; }
-
-ll solve(ll x,ll y) {
-	int xx = x - X + dd + 2, yy = y - Y + dd + 2;
-	ll &ss = seen[xx][yy], &me = memo[xx][yy];
-	if(ss == tmp) return oo;
-	ss = tmp;
-	ll ans = dist(x, y);
-	for(int i = 0; i < D; i++)
-		if(isIn(x + dx[i], y + dy[i])) 
-			ans = min(ans, solve(x + dx[i], y + dy[i]) + 1);
-	return me = ans;
+void show() {
+	for(ll i = dd-5; i < dd+5 ; i++) {
+		for(ll j = dd-5; j < dd+5; j++)
+			printf("(%lld,%lld)%lld ", i-dd,j-dd,best[i][j]);
+		putchar('\n');
+	}
 }
 
 int main() {
+	memset(best, -1, sizeof best);
+	bfs(); //show();
 	while(scanf("%lld %lld", &X, &Y) != 0) {
-		tmp++; printf("%lld\n", solve(X, Y)); 		
+		ll ans = LLONG_MAX;
+		for(int i = -dd; i < dd; i++)
+			for(int j = -dd; j < dd; j++) {
+				ans = min(ans, best[i+dd][j+dd] + dist(i, j));
+				//printf("[%d][%d] best=%lld dist=%lld min %lld\n", i, j, best[i+dd][j+dd], dist(i, j), ans);
+			}
+		printf("%lld\n", ans);	
 	}
 	return 0;
 }
