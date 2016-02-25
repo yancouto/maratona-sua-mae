2ed1331a4a740d6bfb20bf91bd478e2367960e8f
gafeol <gabfeol@gmail.com>
2016-02-25 18:32:40 -0300

    iasdasda

--- Sussu/Codeforces/341/C.cpp
+++ Sussu/Codeforces/341/C.cpp
@@ -25,19 +25,17 @@ int main (){
 	for(int a=0;a<n;a++)
 		cin >> s[a].l >> s[a].r;
 	double res = 0;
-	ll bom = 0;
-	ll tot = 0;
 	for(int a=0;a<n;a++){
 		ll din = 0;
 		int nxt = (a+1)%n;
 		tot = (s[nxt].r - s[nxt].l + 1)*(s[a].r - s[a].l + 1);
 		ll i = s[a].l/p + (s[a].l%p != 0);
 		ll f = s[a].r/p;
+		ll ai  = f-i+1;
 		ll inx = s[nxt].l/p + (s[nxt].l%p != 0);
 		ll fnx = s[nxt].r/p;
-		//bom = (f - i + 1)*(s[nxt].r - s[nxt].l + 1) + ((s[a].r - s[a].l + 1)-(f-i+1))*(fnx - inx + 1);
-		//din = 1000ll*(f-i+1)*(s[nxt].r - s[nxt].l + 1);	
-		res += (double)(1000*(f-i+1)*(f-i+1)) + (double)((double)1000*(s[a].r-s[a].l+1 - (f-i+1))*((double)fnx-inx+1)*((double)fnx-inx+1))/((double)s[nxt].r-s[nxt].l+1);
+		ll anx = fnx - inx + 1;
+		res += 1-((s[a].r - s[a].l+1 - ai)/(s[a].r-s[a].l+1))*((s[nxt].r - s[nxt].l + 1 - anx)/(s[nxt].r - s[nxt].l + 1)); 
 	}
-	printf("%.15f\n", (double)res);
+	printf("%.15f\n", (double)res*1000);
 }
