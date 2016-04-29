659f6c56e52014a3bd8dccbba255c3ee4e653f97
Yan Couto <yancouto@gmail.com>
2016-04-29 15:56:13 -0300

    cf fail

---
+++ CF.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define fst first
+#define snd second
+typedef unsigned long long ull;
+typedef long long ll;
+typedef pair<int, int> pii;
+#define pb push_back
+const ll modn = 1000000007;
+inline ll mod(ll x) { return x % modn; }
+#ifdef ONLINEJUDGE
+#	define LLD "%I64d"
+#	define debug(...) fprintf(stderr, ...)
+#else
+#	define LLD "%lld"
+#	define debug(...) {}
+#endif
+
+int main() {
+	int i, j, n;
+	scanf("%d", &n);
+}
